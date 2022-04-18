fbd8655201248f6887d4a3de450bf09c93d7be08
Alexander Khatsayuk <aahatsayuk@sbercloud.ru>
2022-04-18 12:29:48 +0300

    Replace 'days' with 'expire' option expire cat accept format string like 'days' or 'sec' to format output

--- zbx-ssl.sh
+++ zbx-ssl.sh
@@ -1,6 +1,6 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 # Script version
-VERSION="0.1"
+VERSION="0.2"
 
 HOST=$1
 PROP=$2
@@ -12,15 +12,15 @@ echo "zbx-ssl, version: $VERSION
 
 The script takes three positional parameters:
 1 - hostname or it's IP address (example: www.google.com, 192.168.1.1)
-2 - certificate property to retrieve in 'days', 'serial', 'issuer' and 'fingerprint' (default: days)
+2 - certificate property to retrieve in 'expire', 'serial', 'issuer' and 'fingerprint' (default: days)
 3 - port to connect (default: 443)
 
 Examples:
-# Retrieve days before certificate expires:
-zbx-ssl www.google.com
-42
-# The same as abobe with exact parameter:
-zbx-ssl www.google.com days
+# Retrieve certificate expiration date in unixtime:
+zbx-ssl www.google.com expire
+162356256
+# Expiration date can be formated with prefixes 'days' and 'sec':
+zbx-ssl www.google.com expire:days
 42
 # Retrieve certificate issuer:
 zbx-ssl www.google.com issuer
@@ -31,34 +31,49 @@ exit 0
 }
 
 # Set defaults
-if ! [[ "$PROP" ]]; then PROP="days"; fi
-if ! [[ "$PORT" ]]; then PORT=443; fi
-if ! [[ "$HOST" ]]; then get_help; fi
+if [[ -z "$PROP" ]]; then PROP="expire"; fi
+if [[ -z "$PORT" ]]; then PORT=443; fi
+if [[ -z "$HOST" ]]; then get_help; fi
 
 # Function gets cert expiration date in YYYY-m-d fromat
-function get_cert_days() {
-	CERT_EXPIRE=$(true | openssl s_client -connect $1:$2 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f 2)
-	CERT_EXPIRE_SEC=$(date --date="$CERT_EXPIRE" +%s)
-	echo $((($CERT_EXPIRE_SEC - $(date +%s)) / 86400))
+function get_cert_expire() {
+  CERT_EXPIRE=$(true | openssl s_client -connect $1:$2 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f 2)
+  CERT_EXPIRE_UT=$(date --date="$CERT_EXPIRE" +%s)
+  echo "$CERT_EXPIRE_UT"
 }
 
 # Function returns certificate property
 function get_cert_prop() {
-	CERT_PROP=$(true | openssl s_client -connect $1:$2 2>/dev/null | openssl x509 -noout -$3 -nameopt RFC2253 | sed -r "s/^[0-9a-zA-Z ]*=\s?//")
-	echo $CERT_PROP
+  CERT_PROP=$(true | openssl s_client -connect $1:$2 2>/dev/null | openssl x509 -noout -$3 -nameopt RFC2253 | sed -r "s/^[0-9a-zA-Z ]*=\s?//")
+  echo $CERT_PROP
 }
 
 case "$1" in
-	"-h"|"--help")
-		get_help
-		exit 0
-	;;
+  "-h"|"--help")
+    get_help
+    exit 0
+    ;;
 esac
 
-# Param to displayi
-if [[ "$PROP" == "days" ]]
+# Param to display
+if [[ "$PROP" =~ ^expire ]]
 then
-	echo $(get_cert_days $HOST $PORT) 
+  EXPIRE_TYPE=$(echo "$PROP" | cut -d":" -f 2)
+  EXPIRE=$(get_cert_expire $HOST $PORT)
+  case "$EXPIRE_TYPE" in
+    "days")
+      echo $((($EXPIRE - $(date +%s)) / 86400))
+      exit 0
+      ;;
+    "sec")
+      echo $(($EXPIRE - $(date +%s)))
+      exit 0
+      ;;
+    *)
+      echo "$EXPIRE"
+      exit 0
+      ;;
+   esac
 else
-	echo $(get_cert_prop $HOST $PORT $PROP)
+  echo $(get_cert_prop $HOST $PORT $PROP)
 fi
